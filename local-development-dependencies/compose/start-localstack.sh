6235e257394336090c22eb2bd5805a94a7339ba2
whitewater design <whitewaterdesign>
2025-08-12 14:32:31 +0100

    feat: DF-366 - Add dead-letter RedrivePolicy defaults

--- local-development-dependencies/compose/start-localstack.sh
+++ local-development-dependencies/compose/start-localstack.sh
@@ -44,6 +44,16 @@ aws --endpoint-url=http://localhost:4566 sns create-topic --name forms_entitleme
 
 # queues
 aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name forms_audit_events
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name forms_audit_events-deadletter
+
+# dead letter
+aws --endpoint-url=http://localhost:4566 sqs set-queue-attributes \
+    --queue-url http://sqs.eu-west-2.127.0.0.1:4566/000000000000/forms_audit_events \
+    --attributes '{
+    "RedrivePolicy": "{\"deadLetterTargetArn\":\"http://sqs.eu-west-2.127.0.0.1:4566/000000000000/forms_audit_events-deadletter\",\"maxReceiveCount\":\"3\"}",
+    "ReceiveMessagesWaitTimeSeconds": "20",
+    "VisibilityTimeout": "60"
+}'
 
 # subscriptions
 aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn "arn:aws:sns:eu-west-2:000000000000:forms_manager_events" \

420a976103555e4cfc02444f727250eb0f50e4ad
Jez Barnsley <jbarnsley.github@gmail.com>
2025-08-21 14:06:59 +0100

    Fixed typo

--- test-harness/run-harness.sh
+++ test-harness/run-harness.sh
@@ -42,7 +42,7 @@ COMPOSE_PROJECT_NAME="forms-harness" IMAGE_NAMESPACE="$IMAGE_NAMESPACE" IMAGE_TA
   -f "$ROOT_DIR/local-development-mock-auth/docker-compose.yml" \
   -f "$SCRIPT_DIR/docker-compose.yml" \
   --profile $DESIGNER_PROFILE \
-  --quiet-pull
+  --quiet-pull \
   up -d
 
 ./utils/list-versions.sh

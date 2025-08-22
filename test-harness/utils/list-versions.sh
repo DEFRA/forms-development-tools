4faeb89b8eca9f6f39b895059c93d05a7b01403c
Jez Barnsley <jbarnsley.github@gmail.com>
2025-08-22 08:26:23 +0100

    Test of docker image digest command

--- test-harness/utils/list-versions.sh
+++ test-harness/utils/list-versions.sh
@@ -1,24 +1,23 @@
-CONTAINER_IDS=`docker ps | awk '{print $1}' | grep -v "CONTAINER"`
-for CONTAINER_ID in $CONTAINER_IDS;
-do
-  IMAGE_DETAILS=`docker inspect $CONTAINER_ID | jq '.[] | { name: .Config.Labels."defra.cdp.service.name", digest: .Image } | select(.name != null)'`
-  if [ "$IMAGE_DETAILS" != '' ]; then
-  DIGEST=`echo $IMAGE_DETAILS | jq '.digest' | tr -d '"'`
-  SERVICE_NAME=`echo $IMAGE_DETAILS | jq '.name' | tr -d '"'`
-  echo ServiceName $SERVICE_NAME
-  echo INSPECT `docker inspect $CONTAINER_ID | jq '.[] | { image: .Image, name: .Name, composeImage: .Config.Labels."defra.docker.compose.image", serviceName: .Config.Labels."defra.cdp.service.name" }'`
-  docker inspect $CONTAINER_ID
-  echo ServiceName $SERVICE_NAME >> ../logs.txt
-  docker inspect $CONTAINER_ID >> ../logs.txt
-  CURL_URL="https://registry.hub.docker.com/v2/repositories/defradigital/$SERVICE_NAME/tags"
-  TAG_FILTER=".results[] | { name: .name, digest: .digest } | select (.digest == \"$DIGEST\") | select (.name != \"latest\") | pick(.name)"
+docker images --digests
+IMAGES=`docker images --digests --format 'json'`
+echo Images $IMAGES
 
-  TAG_FILTER3=".results[] | { name: .name, digest: .digest }"
+# for IMAGE in $IMAGES;
+# do
+#   echo Image $IMAGE
+#   echo ImageJSON `echo $IMAGE | jq '.'`
+#   DIGEST=`echo $IMAGE | jq '.digest' | tr -d '"'`
+#   SERVICE_NAME=`echo $IMAGE | jq '.name' | tr -d '"'`
+#   echo ServiceName $SERVICE_NAME
+  # echo ServiceName $SERVICE_NAME >> ../logs.txt
+#   CURL_URL="https://registry.hub.docker.com/v2/repositories/$SERVICE_NAME/tags"
+#   TAG_FILTER=".results[] | { name: .name, digest: .digest } | select (.digest == \"$DIGEST\") | select (.name != \"latest\") | pick(.name)"
 
-  TAG_3=`curl -s $CURL_URL | jq "$TAG_FILTER3"`
-  echo Temp3 $TAG_3
-  echo Temp3 Filter $TAG_FILTER
-  TAG=`curl -s $CURL_URL | jq "$TAG_FILTER" | jq .name`
-  echo $SERVICE_NAME $TAG
-  fi
-done
+#   TAG_FILTER3=".results[] | { name: .name, digest: .digest }"
+
+#   TAG_3=`curl -s $CURL_URL | jq "$TAG_FILTER3"`
+#   echo Temp3 $TAG_3
+#   echo Temp3 Filter $TAG_FILTER
+#   TAG=`curl -s $CURL_URL | jq "$TAG_FILTER" | jq .name`
+#   echo $SERVICE_NAME $TAG
+# done

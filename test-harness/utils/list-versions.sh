4995a626a36f458f5b974661e60e64ba7b7d20e0
Jez Barnsley <jbarnsley.github@gmail.com>
2025-08-26 12:43:47 +0100

    Sort and filter service versions

--- test-harness/utils/list-versions.sh
+++ test-harness/utils/list-versions.sh
@@ -1,19 +1,23 @@
 # Compare downloaded Docker image digest against the digest listed on DockerHub
 # to determine the exact version number (when the image was pulled using tag 'latest')
-IMAGES=`docker images --digests --format '{{ .Repository }} {{ .Digest }}'`
+echo " "
+echo "Image versions"
+echo "=============="
+IMAGES=`docker images --digests --format '{{ .Repository }} {{ .Digest }}'  | sort`
 while IFS= read -r line || [[ -n $line ]]; do
   SERVICE_NAME=`echo $line | awk '{print $1}'`
   DIGEST=`echo $line | awk '{print $2}'`
   # echo ServiceName $SERVICE_NAME
   # echo Digest $DIGEST
-  if [[ "$SERVICE_NAME" == defradigital* ]]; then
+  if [[ "$SERVICE_NAME" == defradigital/forms* ]]; then
     CURL_URL="https://registry.hub.docker.com/v2/repositories/$SERVICE_NAME/tags"
     TAG_FILTER=".results[] | { name: .name, digest: .digest } | select (.digest == \"$DIGEST\") | select (.name != \"latest\") | pick(.name)"
     URL_RESPONSE=`curl -s $CURL_URL`
     URL_RESP_LOWER=`echo $URL_RESPONSE | tr '[:upper:]' '[:lower:]'`
     if [[ $URL_RESP_LOWER != *"not found"* ]]; then
       TAG=`echo $URL_RESPONSE | jq "$TAG_FILTER" | jq .name`
-      echo $SERVICE_NAME $TAG
+      echo ${SERVICE_NAME:13} $TAG
     fi
   fi
 done < <(printf '%s' "$IMAGES")
+echo " "

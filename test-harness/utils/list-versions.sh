cf9fb6686e7e079469aef18d63a48110c4aa910d
Jez Barnsley <jbarnsley.github@gmail.com>
2025-10-21 09:01:18 +0100

    Fixed display of duplicate images

--- test-harness/utils/list-versions.sh
+++ test-harness/utils/list-versions.sh
@@ -13,8 +13,6 @@ OUTPUT_VERSIONS=""
 while IFS= read -r line || [[ -n $line ]]; do
   SERVICE_NAME=`echo $line | awk '{print $1}'`
   DIGEST=`echo $line | awk '{print $2}'`
-  # echo ServiceName $SERVICE_NAME
-  # echo Digest $DIGEST
   if printf '%s\0' "${LATEST_VERSIONS_ARR[@]}" | grep -Fxqz -- $SERVICE_NAME; then
     CURL_URL="https://registry.hub.docker.com/v2/repositories/$SERVICE_NAME/tags"
     TAG_FILTER=".results[] | { name: .name, digest: .digest } | select (.digest == \"$DIGEST\") | select (.name != \"latest\") | pick(.name)"
@@ -22,12 +20,14 @@ while IFS= read -r line || [[ -n $line ]]; do
     URL_RESP_LOWER=`echo $URL_RESPONSE | tr '[:upper:]' '[:lower:]'`
     if [[ $URL_RESP_LOWER != *"not found"* ]]; then
       TAG=`echo $URL_RESPONSE | jq "$TAG_FILTER" | jq .name`
-      OUTPUT_VERSIONS+="${SERVICE_NAME:13} ${TAG}\n"
+      if [[ $PREV_SERVICE_NAME != $SERVICE_NAME ]]; then
+        OUTPUT_VERSIONS+="${SERVICE_NAME:13} ${TAG}\n"
+      fi
     fi
     echo -n "."
   fi
 done < <(printf '%s' "$IMAGES")
 echo " "
-echo -en $OUTPUT_VERSIONS
+echo -en $OUTPUT_VERSIONS | sort -r | sort -u -k1,1
 echo $SPECIFIC_VERSIONS
 echo " "

8f00b3bdf56ee0ede49fd3eff74cf3347de07ce3
Jez Barnsley <jbarnsley.github@gmail.com>
2026-03-18 14:34:54 +0000

    Fixed DLQ configs

--- local-development-dependencies/compose/start-localstack.sh
+++ local-development-dependencies/compose/start-localstack.sh
@@ -77,6 +77,12 @@ aws --endpoint-url=http://localhost:4566 sns create-topic --name forms_runner_su
 # queues
 aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name forms_notify_listener_events
 aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name forms_notify_listener_events-deadletter
+aws --endpoint-url=http://localhost:4566 sqs set-queue-attributes \
+    --queue-url http://sqs.eu-west-2.127.0.0.1:4566/000000000000/forms_notify_listener_events \
+    --attributes '{
+      "RedrivePolicy": "{\"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:forms_notify_listener_events-deadletter\",\"maxReceiveCount\":\"3\"}",
+      "VisibilityTimeout": "60"
+    }'
 
 # subscriptions
 aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn "arn:aws:sns:eu-west-2:000000000000:forms_runner_submission_events" \
@@ -125,6 +131,12 @@ aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn "arn:aws:sns:
 # queues
 aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name forms_sharepoint_listener_events
 aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name forms_sharepoint_listener_events-deadletter
+aws --endpoint-url=http://localhost:4566 sqs set-queue-attributes \
+    --queue-url http://sqs.eu-west-2.127.0.0.1:4566/000000000000/forms_sharepoint_listener_events \
+    --attributes '{
+      "RedrivePolicy": "{\"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:forms_sharepoint_listener_events-deadletter\",\"maxReceiveCount\":\"3\"}",
+      "VisibilityTimeout": "60"
+    }'
 
 # subscriptions
 aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn "arn:aws:sns:eu-west-2:000000000000:forms_runner_submission_events" \
